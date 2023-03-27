d1d4517c8e8b46338b8b65cd9bedfca5557740b6
Andhika M. Wijaya <okumaru07@gmail.com>
2023-03-27 14:50:36 +0700

    add test for contact & tag

---
+++ __tests__/contact.test.ts
@@ -0,0 +1,60 @@
+import request from 'supertest';
+import app from '../src/app';
+
+let res: any;
+const data: Object = {
+  "fullname": "asd18",
+  "email": "asd18",
+  "phone": "1231218",
+  "address": "asd",
+  "tags": ["641dafc828ec00dde4ff7f62"]
+};
+
+describe("Add contact request working well", () => {
+  beforeAll(async () => {
+    res = await request(app)
+      .put("/contact")
+      .send(data);
+  });
+
+  test("Success - Response code is 201", () => {
+    expect(res.statusCode).toEqual(201);
+  });
+
+  test("Success - Response type is json", () => {
+    expect(res.type).toEqual('application/json');
+  });
+});
+
+describe("Edit contact request working well", () => {
+  beforeAll(async () => {
+    res = await request(app)
+      .post(`/contact/${res.body._id}`)
+      .send({
+        "address": "asd18"
+      });
+  });
+
+  test("Success - Response code is 200", () => {
+    expect(res.statusCode).toEqual(200);
+  });
+
+  test("Success - Response type is json", () => {
+    expect(res.type).toEqual('application/json');
+  });
+});
+
+describe("Delete contact request working well", () => {
+  beforeAll(async () => {
+    res = await request(app)
+      .delete(`/contact/${res.body._id}`);
+  });
+
+  test("Success - Response code is 200", () => {
+    expect(res.statusCode).toEqual(200);
+  });
+
+  test("Success - Response type is json", () => {
+    expect(res.type).toEqual('application/json');
+  });
+});
---
+++ __tests__/tag.test.ts
@@ -0,0 +1,56 @@
+import request from 'supertest';
+import app from '../src/app';
+
+let res: any;
+const data: Object = {
+  "name": "asd5"
+};
+
+describe("Add tag request working well", () => {
+  beforeAll(async () => {
+    res = await request(app)
+      .put("/tag")
+      .send(data);
+  });
+
+  test("Success - Response code is 201", () => {
+    expect(res.statusCode).toEqual(201);
+  });
+
+  test("Success - Response type is json", () => {
+    expect(res.type).toEqual('application/json');
+  });
+});
+
+describe("Edit tag request working well", () => {
+  beforeAll(async () => {
+    res = await request(app)
+      .post(`/tag/${res.body._id}`)
+      .send({
+        "name": "asd18"
+      });
+  });
+
+  test("Success - Response code is 200", () => {
+    expect(res.statusCode).toEqual(200);
+  });
+
+  test("Success - Response type is json", () => {
+    expect(res.type).toEqual('application/json');
+  });
+});
+
+describe("Delete tag request working well", () => {
+  beforeAll(async () => {
+    res = await request(app)
+      .delete(`/tag/${res.body._id}`);
+  });
+
+  test("Success - Response code is 200", () => {
+    expect(res.statusCode).toEqual(200);
+  });
+
+  test("Success - Response type is json", () => {
+    expect(res.type).toEqual('application/json');
+  });
+});
